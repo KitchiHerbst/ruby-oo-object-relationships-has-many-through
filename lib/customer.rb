cd04a0b61c598f87fba8d87c656b991516482105
Triston <tristonherbst97@gmail.com>
2021-02-28 15:54:44 -0600

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
+
 class Customer
+
+    @@all = []
+
+    attr_accessor :name, :age
+
+    def initialize(name,age)
+        @name=name
+        @age=age
+        @@all << self
+    end
   
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter,total,tip)
+        Meal.new(waiter,self,total,tip)
+    end
+
+    def meals
+        Meal.all.select {|meal|self == meal.customer}
+        #binding.pry
+    end
+
+    def waiters
+        new_array = []
+        Meal.all.each do |meal|
+            if self == meal.customer
+                new_array << meal.waiter
+            end
+        end
+        new_array
+        #Meal.all.select {|meal| meal.waiter if self == meal.customer end}
+        #binding.pry
+    end
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,45 @@
+
+
 class Waiter
+
+    @@all = []
+
+    attr_accessor :name, :exp
+
+    def initialize(name,exp)
+        @name=name
+        @exp=exp
+        @@all << self
+    end
+  
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer,total,tip)
+        Meal.new(self,customer,total,tip)
+    end
+
+    def meals
+        Meal.all.select {|meal|self ==meal.waiter}
+    end
+
+    def best_tipper
+        biggest_tip = 0
+        Meal.all.each do |meal|
+            if meal.tip > biggest_tip
+                biggest_tip = meal.tip
+                #binding.pry
+            end
+        end
+        #binding.pry
+        Meal.all.each do |meal|
+            if biggest_tip == meal.tip
+                return meal.customer
+            end
+        end
+    end
+
+
 
 end
